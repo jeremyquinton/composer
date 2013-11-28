b3506d671628eb8fa9ff855f2ecef7fc55cc7ca6
Mark Wilson <mark@89allport.co.uk>
2013-11-28 12:11:45 +0000

    Tweak to metadata.rb to allow upload to chef server

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,5 @@
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
-description      "Installs/Configures hostsfile-writer"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+description      "Installs composer executable"
 version          "0.0.1"
